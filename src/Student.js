1b38fc9c9953975d3c5e6db77fabd346bf39ff3d
YOUR NAME <62856817+AllisOrder@users.noreply.github.com>
2021-09-09 15:27:11 -0400

    minor changes

--- src/Student.js
+++ src/Student.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const Student = (props) => {
-  console.log( props.students );
-  return <div>{/* <h3>{props.studentData.students}</h3> */}</div>;
+  console.log(props.studentData.students);
+  console.log(props.students.name);
+  return (
+    <div>
+      <h3>{props.students.name}</h3>
+    </div>
+  );
 };
 
 export default Student;
