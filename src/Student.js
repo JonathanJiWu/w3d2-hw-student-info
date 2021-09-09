28a97beff315ed1a485627978eca1ecae2ddff70
YOUR NAME <62856817+AllisOrder@users.noreply.github.com>
2021-09-08 22:51:34 -0400

    add data, comps

---
+++ src/Student.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import students from './data'
+
+const Student = () => {
+    
+    return (
+        <div>
+            students.map(i){
+i.name
+            }
+        </div>
+    )
+}
+
+export default Student
